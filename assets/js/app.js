6bb23b2676067e991ccf037df197addd7fbe49f3
Mariko Aburai <aburmari@gmail.com>
2018-11-02 18:22:12 +0900

    削除と完了ボタンを追加

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,33 @@
 // - Todoを画面に追加する処理
-//#addボタンを押したら、input内のvalueを変数に入れて、ul#not-yetにli要素で表示する
+let removeIcon = '<i class="fas fa-trash-alt fa-2x"></i>';
+let doneIcon = '<i class="fas fa-check-circle fa-2x"></i>'
 
+
+//#addボタンを押したら、input内のvalueを変数に入れて、ul#not-yetにli要素で表示する
 let add = document.getElementById('add');
 add.addEventListener('click', function(){
     let taskName = document.getElementById('task').value;
     let li = document.createElement('li');
-    li.append('taskName');
+    li.append(taskName);
     document.getElementById('not-yet').appendChild(li);
+//アイコンを設置したい。liにbuttons要素を用意
+    let buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+    li.appendChild(buttons);
+//.removeをつけたbutton要素に削除アイコンを入れる
+    let remove = document.createElement('button');
+    remove.classList.add('remove');
+    remove.innerHTML = removeIcon;
+    buttons.appendChild(remove);
+//.doneをつけたbuttonに完了ボタンをつける
+    let done = document.createElement('button');
+    done.classList.add('done');
+    done.innerHTML = doneIcon;
+    buttons.appendChild(done);
+
+
+
+
 
 })
 
