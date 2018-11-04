9b443b2f7c75cdadcc240c52f96008f431edf175
Mariko Aburai <aburmari@gmail.com>
2018-11-04 14:34:25 +0900

    2回目　関数化とDB保存呼び出し、挙動確認で表示タグに間違え有り。修正。

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ let doneIcon ='<i class="fas fa-clipboard-check fa-2x"></i>';
 
 let data;
 
+
 if (localStorage.getItem('todoList')){
   data = JSON.parse(localStorage.getItem('todoList'));
   rederTodoList();
@@ -28,11 +29,12 @@ let add = document.getElementById('add');
 
 
 // 関数------------------------------
-function rederTodoList(data){
+function rederTodoList(){
   for (let taskName of data.li){
     addTaskToDOM(taskName, false);
   }
   for (let taskName of data.done){
+      // console.log(taskName);
     addTaskToDOM(taskName, true);
   }
 }
@@ -40,25 +42,27 @@ function rederTodoList(data){
 function addTaskToDOM(taskName, isDone){
   let list;
   if (isDone) {
-    list = document.createElement('done');
+    list = document.getElementById('done');
   } else {
     list = document.getElementById('not-yet');
   }
 
   let li = document.createElement('li');
   li.append(taskName);
+    // console.log(li);
   list.appendChild(li);
+    console.log(list);
 
   //Iconを用意して入れる場所を作り、liに入れる
   let remove = document.createElement('button');
   remove.classList.add('remove');
   remove.innerHTML = removeIcon;
-      console.log(remove);
+      // console.log(remove);
 
   let done = document.createElement('button')
   done.classList.add('done');
   done.innerHTML = doneIcon;
-      console.log(done);
+      // console.log(done);
 
   let buttons = document.createElement('div');
   buttons.classList.add('buttons');
@@ -75,23 +79,32 @@ function addTaskToDOM(taskName, isDone){
 
 function removeTask() {
   let task = this.parentNode.parentNode;
-  let id = this.parentNode.id;
+    // console.log(task);
+    // console.log(`削除ボタンが押された内容${task}`);
+
+  let id = task.parentNode.id;
+    // console.log(id);
+    // console.log(`${id}から消す`);
   let value = task.textContent;
-  task.remove();
 //配列とDBから削除
   if (id === 'not-yet'){
     data.li.splice(data.li.indexOf(value), 1);
+      // console.log(`配列の変更${data.li}`);
     } else {
     data.done.splice(data.done.indexOf(value), 1);
   }
+  task.remove();
   dataObjectUpdated();
+    console.log('data update done');
 }
 
 function doneTask() {
     let task = this.parentNode.parentNode;
-    let id = this.parentNode.id;
+    let id = task.parentNode.id;
 
     if (id === 'done') {
+      console.log(data);
+
       return;
     }
 
@@ -100,8 +113,10 @@ function doneTask() {
     data.li.splice(data.li.indexOf(value), 1);
     data.done.push(value);
     dataObjectUpdated();
+
   }
 
 function dataObjectUpdated() {
   localStorage.setItem('todoList', JSON.stringify(data));
+  console.log(data);
 }
